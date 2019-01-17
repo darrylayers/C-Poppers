5ac1141684ff192e76bfbd9cb253612288a25d3f
darrylayers <ayersda@appstate.edu>
2019-01-17 16:13:06 -0500

    Sprites pop when clicked

--- pop.c
+++ pop.c
@@ -9,7 +9,7 @@
 //----------------------------------------------------------------------------------
 
 #define NUM_MAX_POPPERS 1000
-#define NUM_POPPERS 50
+#define NUM_POPPERS 10
 #define POPPER_SIZE 20
 
 //----------------------------------------------------------------------------------
@@ -21,6 +21,7 @@ typedef struct Sprite{
     Vector2 position;
     Color color;
     double speed;
+    bool alive;
 } Sprite;
 
 //------------------------------------------------------------------------------------
@@ -30,6 +31,7 @@ typedef struct Sprite{
 static int screenWidth = 1280;
 static int screenHeight = 720;
 static Sprite poppers[NUM_MAX_POPPERS];
+static int xPos[NUM_MAX_POPPERS];           // used to make sure sprites do not spawn in same spot
 static int clicks = 0;
 
 
@@ -40,7 +42,6 @@ static void InitGame(void);         // Initialize game
 static void UpdateGame(void);       // Update game (one frame)
 static void DrawGame(void);         // Draw game (one frame)
 static void UpdateDrawFrame(void);  // Update and Draw (one frame)
-static int getRandom(int bound);    // Get a random int between 0 and bound
 
 //------------------------------------------------------------------------------------
 // Main function and game loop
@@ -65,14 +66,6 @@ int main()
 }
 
 //------------------------------------------------------------------------------------
-// Get a random int between 0 and bound
-//------------------------------------------------------------------------------------
-int getRandom(int bound) 
-{ 
-    return (rand() % (bound));
-} 
-
-//------------------------------------------------------------------------------------
 // Initialize game
 //------------------------------------------------------------------------------------
 void InitGame(void)
@@ -82,20 +75,19 @@ void InitGame(void)
     {
         Sprite sprite;
         sprite.id = i;
-        Vector2 position = {getRandom(1280), -60};
+        sprite.alive = 1;
+        int rand = GetRandomValue(0, 1280);
+        
+        
+        Vector2 position = {rand, -60};  
         sprite.position = position;
-        if ((i % 2) == 0) {
-            sprite.color = PINK;
-            sprite.speed = 1.0; 
-        }
-        else if (i == 10) {
-            sprite.color = RED;
-            sprite.speed = 1.3;
-        } else {
-           sprite.color = DARKBLUE;
-           sprite.speed = 1.8;              
-        }
-        poppers[i] = sprite;       
+        sprite.color = RED;
+        sprite.speed = 1.3;
+        poppers[i] = sprite;
+
+        printf("Created sprite %d \n", sprite.id);
+        fflush(NULL);
+        
     }
 }
 
@@ -106,53 +98,45 @@ void UpdateGame(void){
     
     for(int i = 0; i < NUM_POPPERS; i++) {
         
-        Sprite sprite = poppers[i];
-        double modifier = 1.0;
-        Vector2 position = sprite.position;
-        Color color = sprite.color;
-        
+        Vector2 position = poppers[i].position;
+
         //Check if the ball was clicked
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && 
-        (CheckCollisionPointCircle(GetMousePosition(), position, POPPER_SIZE))){
+        (CheckCollisionPointCircle(GetMousePosition(), position, POPPER_SIZE))){    
             
-            if (ColorToInt(color) == ColorToInt(PINK)) {
-                sprite.color = RED;   
-                sprite.speed = 4.8;              
-            } else if (ColorToInt(color) == ColorToInt(RED)){
-                sprite.color = DARKBLUE;
-                modifier = 1.8;                 
-            } else {
-                sprite.color = PINK;
-                sprite.speed = 1.0;  
-            }
+            poppers[i].alive = 0;
             clicks++;
-        }        
-  
-        //Check to see if impacted wall
-        if (sprite.position.x >= 1260 && sprite.position.y <= 700) {
-            Vector2 newPos = {(sprite.position.x), (sprite.position.y + 1*(sprite.speed))};
-            sprite.position = newPos; 
-            poppers[i] = sprite;            
-        } 
-        else if (sprite.position.y >= 700) {
-            Vector2 newPos = {(sprite.position.x), (sprite.position.y)};
-            sprite.position = newPos; 
-            poppers[i] = sprite;            
-        }          
-        else if ((sprite.position.x >= 1260) && (sprite.position.y >= 700)){
-            Vector2 newPos = {(sprite.position.x), (sprite.position.y)};
-            sprite.position = newPos; 
-            poppers[i] = sprite;     
-        } else {
-            //If no impact, behave normally
-            Vector2 newPos = {(sprite.position.x), (sprite.position.y + 1*(sprite.speed))};
-            sprite.position = newPos; 
-            poppers[i] = sprite;
+        }  
+        else {      
+
+            //Check to see if impacted wall
+            if (poppers[i].position.x >= 1260 && poppers[i].position.y <= 700) {
+                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + poppers[i].speed)};
+                poppers[i].position = newPos;           
+            } 
+            else if (poppers[i].position.y >= 700) {
+                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
+                poppers[i].position = newPos;        
+                }          
+            else if ((poppers[i].position.x >= 1260) && (poppers[i].position.y >= 700)){
+                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
+                poppers[i].position = newPos;  
+            } else {
+                //If no impact, behave normally
+                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + 1*(poppers[i].speed))};
+                poppers[i].position = newPos;
+            }
+        
         }
         char score[3];
         sprintf(score, "%d", clicks);
         DrawText("SCORE: ", 0, 0, 25, BLACK); 
-        DrawText(score, 100, 0, 25, BLACK);         
+        DrawText(score, 100, 0, 25, BLACK);
+
+        if (clicks >= NUM_POPPERS) {
+            DrawText("YOU WIN!", 640,  360, 70, BLACK);
+        }
+        
     }
 }
 
@@ -165,7 +149,12 @@ void DrawGame(void){
         for(int i = 0; i < NUM_POPPERS; i++) 
         {
             Sprite sprite = poppers[i];
-            DrawCircleV(sprite.position, POPPER_SIZE, sprite.color);
+            //printf("%d \n", sprite.alive);
+            //fflush(NULL);
+            if (sprite.alive == 1) {
+              DrawCircleV(sprite.position, POPPER_SIZE, sprite.color);              
+            }
+
         }
 
     EndDrawing();
@@ -179,4 +168,3 @@ void UpdateDrawFrame(void)
     UpdateGame();
     DrawGame();
 }
-
