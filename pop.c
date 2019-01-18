01bb405e8ffd7e0ce285c9f3094c2563cd0b83d2
darrylayers <ayersda@appstate.edu>
2019-01-17 20:08:09 -0500

    Basic game works now

--- pop.c
+++ pop.c
@@ -32,6 +32,8 @@ static int screenWidth = 1280;
 static int screenHeight = 720;
 static int clicks = 0;
 static int aliveSprites = 0;
+static bool gameOver = 0;
+static bool win = 0;
 static Sprite poppers[NUM_MAX_POPPERS];
 
 //------------------------------------------------------------------------------------
@@ -42,6 +44,7 @@ static void UpdateGame(void);       // Update game (one frame)
 static void DrawGame(void);         // Draw game (one frame)
 static void UpdateDrawFrame(void);  // Update and Draw (one frame)
 static Vector2 verifyNoCollision(Vector2 position);
+static bool checkGameOver(void);
 
 //------------------------------------------------------------------------------------
 // Main function and game loop
@@ -70,13 +73,18 @@ int main()
 //------------------------------------------------------------------------------------
 void InitGame(void)
 {
+    clicks = 0;
+    aliveSprites = 0;
+    gameOver = 0;
+    win = 0;
+    
     for(int i = 0; i < NUM_POPPERS; i++) 
     {
         Sprite sprite;
         sprite.id = i;
         sprite.speed = 1.3;
         sprite.alive = 1;
-        sprite.color = RED;
+        sprite.color = DARKBLUE;
         
         // Generate random x value
         int rand = GetRandomValue(50, 1230);
@@ -97,49 +105,69 @@ void InitGame(void)
 //------------------------------------------------------------------------------------
 void UpdateGame(void){
     
-    for(int i = 0; i < NUM_POPPERS; i++) {
+    if(checkGameOver() && !win) {
+        DrawText("YOU LOSE", screenWidth / 2 - 160,  screenHeight / 2 - 165, 70, BLACK); 
+    } 
         
-        Vector2 position = poppers[i].position;
-
-        //Check if the ball was clicked
-        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && 
-        (CheckCollisionPointCircle(GetMousePosition(), position, POPPER_SIZE))){    
+        for(int i = 0; i < NUM_POPPERS; i++) {
             
-            poppers[i].alive = 0;
-            clicks++;
-            
-        }  
-        else {      
+            Vector2 position = poppers[i].position;
 
-            //Check to see if impacted wall
-            if (poppers[i].position.x >= 1260 && poppers[i].position.y <= 700) {
-                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + poppers[i].speed)};
-                poppers[i].position = newPos;           
-            } 
-            else if (poppers[i].position.y >= 700) {
-                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
-                poppers[i].position = newPos;        
-                }          
-            else if ((poppers[i].position.x >= 1260) && (poppers[i].position.y >= 700)){
-                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
-                poppers[i].position = newPos;  
-            } else {
-                //If no impact, behave normally
-                Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + 1*(poppers[i].speed))};
-                poppers[i].position = newPos;
+            //Check if the ball was clicked
+            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && 
+            (CheckCollisionPointCircle(GetMousePosition(), position, POPPER_SIZE)) && !checkGameOver()){    
+
+                if(poppers[i].alive == 1){
+                    poppers[i].alive = 0;
+                    clicks++;    
+                }
+            }  
+            else {      
+
+                //Check to see if impacted wall
+                if (poppers[i].position.x >= 1260 && poppers[i].position.y <= 700) {
+                    Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + poppers[i].speed)};
+                    poppers[i].position = newPos;           
+                } 
+                else if (poppers[i].position.y >= 700) {
+                    Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
+                    poppers[i].position = newPos;        
+                    }          
+                else if ((poppers[i].position.x >= 1260) && (poppers[i].position.y >= 700)){
+                    Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y)};
+                    poppers[i].position = newPos;  
+                } else {
+                    //If no impact, behave normally
+                    Vector2 newPos = {(poppers[i].position.x), (poppers[i].position.y + 1*(poppers[i].speed))};
+                    poppers[i].position = newPos;
+                }
+            
             }
-        
-        }
-        char score[3];
-        sprintf(score, "%d", clicks);
-        DrawText("SCORE: ", 0, 0, 25, BLACK); 
-        DrawText(score, 100, 0, 25, BLACK);
+            char score[3];
+            sprintf(score, "%d", clicks);
+            DrawText("SCORE: ", 0, 0, 25, BLACK); 
+            DrawText(score, 100, 0, 25, BLACK);
 
-        if (clicks >= NUM_POPPERS) {
-            DrawText("YOU WIN!", 640,  360, 70, BLACK);
-        }
-        
-    }
+            if (clicks >= NUM_POPPERS) {
+                win = 1;
+                DrawText("YOU WIN!", screenWidth / 2 - 160,  screenHeight / 2 - 165, 70, BLACK);
+            }
+            
+            if((gameOver) || (clicks >= NUM_POPPERS)){
+                DrawText("Restart?", screenWidth / 2 - 160,  screenHeight / 2, 70, BLACK);
+
+                DrawRectangleLines(screenWidth / 2 - 170, screenHeight / 2, 350, 70, BLACK);
+
+                Rectangle rectangle = {screenWidth / 2 - 170, screenHeight / 2, 350, 70};
+                
+                Vector2 restartPos = {screenWidth / 2 - 160,screenHeight / 2};
+                if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && 
+                (CheckCollisionPointRec(GetMousePosition(), rectangle))){    
+                    InitGame();    
+                } 
+            }
+            
+    }    
 }
 
 //------------------------------------------------------------------------------------
@@ -149,6 +177,7 @@ void DrawGame(void){
     BeginDrawing();
     
         ClearBackground(RAYWHITE);
+        DrawLine(0, 600, 1280, 600, RED);   
         
         for(int i = 0; i < NUM_POPPERS; i++) 
         {
@@ -200,4 +229,22 @@ Vector2 verifyNoCollision(Vector2 position) {
     }
    
     return position;
+}
+
+bool checkGameOver() {
+    int counter = aliveSprites;
+    for (int i = 0; i <= aliveSprites; i++) {
+        int pos = poppers[i].position.y;
+        //printf(" %d %d \n", poppers[i].id, pos);
+        //fflush(NULL);
+        if (pos > 580){
+            counter--;
+        } 
+    }
+    
+    if (counter == 0) {
+        gameOver = 1;
+        return 1;
+    }
+    return 0;
 }
